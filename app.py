0f958c7dd0583d671de88341b77e99a68426bb31
andre <andrebeet1@gmail.com>
2025-08-21 19:59:35 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ migrate = Migrate(app, db)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(128), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
 class Membre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
